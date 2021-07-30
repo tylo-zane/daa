5d36bfd3cfb7d440676b0fd57656664e98d469ca
tylo-zane <43626511+tylo-zane@users.noreply.github.com>
2021-07-29 17:29:41 -0700

    made checkboxes clickable

--- index.js
+++ index.js
@@ -9,12 +9,22 @@
     function initialize() {
         let node = document.querySelectorAll(".node");
         for (let i = 0; i < node.length; i++) {
-            node[i].onclick = function() {
-              this.classList.toggle('checked');
-            }
+            node[i].addEventListener("click", toggleCheck);
+            let box = node[i].querySelector(".check");
+            addCheck(box);
           }
     }
- 
+
+    function toggleCheck() {
+        this.classList.toggle("checked");
+    }
+
+    function addCheck(box) {
+        let image = document.createElement("img");
+        image.src = "img/check.png";
+        image.alt = "A check mark";
+        box.appendChild(image);
+    }
  
  })();
  
